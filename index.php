f100cc97ed61e62b0fb21a5693e739328643b1f7
Sarthak Sadh <sarthaksadh@Sarthaks-MacBook-Pro-427.local>
2019-01-04 23:16:54 +0530

    setup

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+if(isset($_POST['state'])){
+    $myfile = fopen("data.txt", "w") or die("error");
+    $data=$_POST['state'];
+    fwrite($myfile, $data);
+    fclose($myfile);
+    echo "success";
+ 
+}
+
+if(isset($_GET['led'])){
+
+    $myfile = fopen("data.txt", "r") or die("error");
+    echo fread($myfile,filesize("data.txt"));
+    fclose($myfile);
+}
+
+
+?>
